9094f30639411b0b25b0a4a49028fe938aa45a92
gr8monk3ys <lorenzosca7@protonmail.ch>
2024-03-18 19:35:30 -0700

    modifications

--- follow.py
+++ follow.py
@@ -7,56 +7,48 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import parameters, csv, os.path, time, random
 
-service = Service(executable_path="C:\\Users\\JoeG&M\\Downloads\\chromedriver-win64\\chromedriver.exe")
-driver = webdriver.Chrome(service=service)
-
-def sleep(seconds):
-    time.sleep(seconds)
-
 def login(username, password):
     login_button = driver.find_element(By.CLASS_NAME, 'standalone-flow-button')
     
     username_field.send_keys(username)
     password_field.send_keys(password)
     login_button.click()
-    
-    # Wait for the login to complete (can be adjusted as necessary)
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'element unique to logged in users'))
-    )
 
-def is_logged_in():
-    driver.get("https://letterboxd.com/")
-    try:
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'element unique to logged in users'))
-        )
-        return True
-    except:
-        return False
-
-def follow():
-    driver.get("https://letterboxd.com/kurstboy/followers/")
-    count = 0
-    try:
-        elements = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'ajax-click-action button'))
-        )
+def follow(till_page, driver):
+    for page in range(20, till_page + 1):
+        url = "https://letterboxd.com/kurstboy/followers/page/" + str(page) + "/"
+        driver.get(url)
+        print(f'\nINFO: Checking followers on page {page}')
+        html = driver.find_element(By.TAG_NAME, 'html')
+        html.send_keys(Keys.END)
+        time.sleep(3)
+        elements = WebDriverWait(driver, 5).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[data-recaptcha-action=follow]:not([style*="display: none"])')))
+        # try:
         for el in elements:
-            sleep(random.randint(1, 2))
-            el.click()
-            count += 1
-            if count == 25:
-                break
+            time.sleep(random.randint(2, 3))  # Simulate human-like interaction delays
+            driver.execute_script("arguments[0].scrollIntoView();", el)
+            driver.execute_script("arguments[0].click();", el)
+            # el.click()
+            time.sleep(1.5)
+                # count += 1
+                # print(f"{count} ) FOLLOWED: Successfully followed")
+                # if count == 25:  # Limit to 25 follow actions to avoid being too aggressive
+                #     print("INFO: Reached follow limit of 25 for this session.")
+                #     return
 
-        next_button = driver.find_element(By.CLASS_NAME, 'next')
-        if next_button:
-            next_button.click()
-            time.sleep(random.randint(1, 4))
-    except Exception as e:
-        print("An error occurred:", e)
+            # next_button = driver.find_element(By.CSS_SELECTOR, 'a.next')
+            # if next_button.is_displayed():
+            #     next_button.click()
+            #     time.sleep(random.randint(1, 4))
+            #     page += 1
+            # else:
+            #     print("INFO: No more pages to navigate. Finished following.")
+            #     break
+        # except Exception as e:
+        #     print(f"An error occurred on page {page}: {e}")
+        #     break
 
-follow()
 if __name__ == "__main__":
 
     service = Service()
@@ -67,22 +59,15 @@ if __name__ == "__main__":
         driver.get("https://letterboxd.com/sign-in/")
         username_field = driver.find_element(By.ID, 'field-username')
         password_field = driver.find_element(By.ID, 'field-password')
-        login(username_field, password_field)
-        time.sleep(10)
+        login(parameters.username, parameters.password)
+        time.sleep(2)
 
         # CSV file loging
-        file_name = parameters.file_name
-        file_exists = os.path.isfile(file_name)
-        writer = csv.writer(open(file_name, 'a'))
-        if not file_exists: writer.writerow(['Connection Summary'])
-        ignore_list = parameters.ignore_list
-        if ignore_list:
-            ignore_list = [i.strip() for i in ignore_list.split(',') if i]
-        else:
-            ignore_list = []
-
-        # Search
-        follow()
+        # file_name = parameters.file_name
+        # file_exists = os.path.isfile(file_name)
+        # writer = csv.writer(open(file_name, 'a'))
+        # if not file_exists: writer.writerow(['Connection Summary'])
+        follow(parameters.till_page, driver)
     except KeyboardInterrupt:
         print("\n\nINFO: User Canceled\n")
     except Exception as e:
--- parameters.py
+++ parameters.py
@@ -1,6 +1,6 @@
 file_name = 'connections.csv'
-linkedin_username = 'gr8monk3ys'
-linkedin_password = 'Scaturchio8'
+username = 'gr8monk3ys'
+password = 'Scaturchio8'
 keywords = 'IBM'
 ignore_list = 'Person1,Person2'
 till_page = 30
