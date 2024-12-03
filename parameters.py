1f13cce853bd58b8efddd2d00f04d0b3a5df0c3f
Joe | GJoeG&MM <joe@gmtri.com>
2024-12-03 13:51:23 -0800

    config: update base configuration parameters

--- parameters.py
+++ parameters.py
@@ -1,6 +1,8 @@
-file_name = 'connections.csv'
-username = 'gr8monk3ys'
-password = 'Scaturchio8'
-keywords = 'IBM'
-ignore_list = 'Person1,Person2'
-till_page = 30
+username = 'gr8monk3ys'  # Your Letterboxd username
+password = 'Scaturchio8'  # Your Letterboxd password
+file_name = 'connections.csv'  # File to log followed users
+till_page = 30  # Number of pages to process
+base_url = 'https://letterboxd.com/jaragon23/followers/'  # Base URL for user list
+min_delay = 2  # Minimum delay between actions in seconds
+max_delay = 5  # Maximum delay between actions in seconds
+max_follows_per_session = 100  # Maximum number of follows per session
